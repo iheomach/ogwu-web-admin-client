d253109ab7bab6166b8cf5a985deeea6175c140a
iheomach <iheomacr7@gmail.com>
2026-05-21 10:55:21 -0700

    logo change

--- src/components/ui/OgwuLogo.tsx
+++ src/components/ui/OgwuLogo.tsx
@@ -1,7 +1,7 @@
 export function OgwuLogo({ size = 36 }: { size?: number }) {
   return (
     <img
-      src="/ogwu-logo-ios.png"
+      src="/ogwu-mark.png"
       alt="Ogwu"
       style={{ width: size, height: size, borderRadius: size * 0.22, display: 'block', flexShrink: 0 }}
     />
